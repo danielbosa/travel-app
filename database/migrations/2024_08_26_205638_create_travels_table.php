24debf77e9edb050e65f1088e652dbdeb305d684
danielbosa <danielbosa95@gmail.com>
2024-08-26 23:02:22 +0200

    Travels: migration - setting up

--- database/migrations/2024_08_26_205638_create_travels_table.php
+++ database/migrations/2024_08_26_205638_create_travels_table.php
@@ -12,8 +12,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('travels', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id(); // Crea una colonna auto-incrementante 'id' come chiave primaria
+
+            // Colonne aggiuntive
+            $table->string('name'); // VARCHAR(255)
+            $table->text('description')->nullable(); // TEXT
+            $table->string('image')->nullable(); // VARCHAR(255)
+            $table->date('date_start')->nullable(); // DATE
+
+            // Colonna chiave esterna
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+
+            $table->timestamps(); // Aggiunge le colonne 'created_at' e 'updated_at'
         });
     }
 
