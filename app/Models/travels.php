0ad8d651c989edaa1be590d460f50313ad195337
danielbosa <danielbosa95@gmail.com>
2024-08-26 23:06:30 +0200

    one2many rel with users

--- app/Models/travels.php
+++ app/Models/travels.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class travels extends Model
 {
     use HasFactory;
+
+    // one2many rel with user
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
