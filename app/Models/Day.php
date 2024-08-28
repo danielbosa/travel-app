f20eb44e1b4b86b7842ca5883b27e740132747bd
danielbosa <danielbosa95@gmail.com>
2024-08-28 15:56:28 +0200

    Set up relations with Travels

--- app/Models/Day.php
+++ app/Models/Day.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class days extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    // one2many with travel: secondary
+    public function travel(): BelongsTo
+    {
+        return $this->belongsTo(Travel::class);
+    }
 }
