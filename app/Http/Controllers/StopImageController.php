0ab500da55d618ea0e4e6b4af6a2216dd641c469
danielbosa <danielbosa95@gmail.com>
2024-08-28 17:47:06 +0200

    Stop_images file setup

---
+++ app/Http/Controllers/StopImageController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Stop_image;
+use Illuminate\Http\Request;
+
+class StopImageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Stop_image $stop_image)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Stop_image $stop_image)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Stop_image $stop_image)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Stop_image $stop_image)
+    {
+        //
+    }
+}
