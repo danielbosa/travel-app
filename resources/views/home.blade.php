0f00fec31478b37dd351e852c6ed1bd9e8ddeac3
danielbosa <danielbosa95@gmail.com>
2024-08-29 20:36:10 +0200

    Home (user not logged in)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 @section('content')
-<section class="hero">
-    <img src="https://www.adobe.com/content/dam/cc/us/en/creative-cloud/photography/discover/landscape-photography/CODERED_B1_landscape_hero-img_900x420.jpg.img.jpg" alt="">
-</section>
+    <section class="hero">
+        <img src="https://www.adobe.com/content/dam/cc/us/en/creative-cloud/photography/discover/landscape-photography/CODERED_B1_landscape_hero-img_900x420.jpg.img.jpg" alt="">
+    </section>
     <section class="container">
         <h1 class="fw-bold my-4">Il tuo viaggio a portata di mano</h1>
         <h2 class="fs-4 mb-3">Organizza, pianifica e vivi le tue vacanze con la nostra Travel App!</h2>
@@ -22,4 +22,9 @@
         </p>
         <p class="fw-bold">Inizia ora a pianificare il tuo prossimo viaggio e rendilo indimenticabile con la tua Travel App!</p>
     </section>
+    <section id="cta">
+        <a class="btn-cta" href="{{ route('register') }}">{{ __('Registrati') }}</a>
+        <a class="btn-cta" href="{{ route('login') }}">{{ __('Login') }}</a>
+    </section>
 @endsection
+
