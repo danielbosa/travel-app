d6d94d7eeb1c8545871a1de8c4721fee6eacaf7e
danielbosa <danielbosa95@gmail.com>
2024-08-29 18:33:26 +0200

    Hero image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.app')
 @section('content')
+<section class="hero">
+    <img src="https://www.adobe.com/content/dam/cc/us/en/creative-cloud/photography/discover/landscape-photography/CODERED_B1_landscape_hero-img_900x420.jpg.img.jpg" alt="">
+</section>
     <section class="container">
-        <h1>Section title</h1>
+        <h1>Il tuo viaggio a portata di mano</h1>
         <p>section content</p>
     </section>
 @endsection
