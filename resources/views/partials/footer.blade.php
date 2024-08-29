4e832c055866b3f02e831c0650207c42b3415fb5
danielbosa <danielbosa95@gmail.com>
2024-08-29 20:26:44 +0200

    Footer component

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer container">
+    <p>© 2024 Travel App. All rights reserved.</p>
+    <ul class="footer__links">
+        <li><a href="/privacy">Privacy Policy</a></li>
+        <li><a href="/terms">Termini di Servizio</a></li>
+    </ul>
+</footer>
