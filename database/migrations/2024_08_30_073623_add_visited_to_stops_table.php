49b8507080d815446cfd4f7a55be7dbf1192c0cd
danielbosa <danielbosa95@gmail.com>
2024-08-30 09:37:05 +0200

    Migration to add col to stops table

---
+++ database/migrations/2024_08_30_073623_add_visited_to_stops_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('stops', function (Blueprint $table) {
+            $table->boolean('visited')->default(false); // Aggiungi la colonna 'visited' con valore predefinito 'false'
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('stops', function (Blueprint $table) {
+            $table->dropColumn('visited'); // Rimuovi la colonna 'visited' se necessario
+        });
+    }
+};
