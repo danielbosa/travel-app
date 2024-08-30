6af40904b23a53506dd8cc7d92ce913978f0e66f
danielbosa <danielbosa95@gmail.com>
2024-08-30 10:44:11 +0200

    Seeding db, stop_images table

--- database/seeders/StopImageSeeder.php
+++ database/seeders/StopImageSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class StopImageSeeder extends Seeder
 {
@@ -12,6 +13,18 @@ class StopImageSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $images = [
+            ['stop_id' => 1, 'image_path' => 'piazza_signoria_firenze.jpg'],
+            ['stop_id' => 2, 'image_path' => 'cattedrale_santa_maria_del_fiore_firenze.jpg'],
+            ['stop_id' => 3, 'image_path' => 'ponte_vecchio.jpg'],
+            ['stop_id' => 4, 'image_path' => 'piazza_grande_montepulciano.jpg'],
+            ['stop_id' => 5, 'image_path' => 'duomo_montepulciano.jpg'],
+            ['stop_id' => 6, 'image_path' => 'cantina_nobile.jpg'],
+            ['stop_id' => 7, 'image_path' => 'piazza_del_campo_siena.jpg'],
+            ['stop_id' => 8, 'image_path' => 'duomo_siena.jpg'],
+            ['stop_id' => 9, 'image_path' => 'torre_del_mangia_siena.jpg'],
+        ];
+
+        DB::table('stop_images')->insert($images);
     }
 }
